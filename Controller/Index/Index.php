91ec86beb1eff01178493b01b1c2b1ab7bd8e235
Pabitra Kumar Rout <pabitra.rout@jispl.com>
2019-12-20 20:58:08 +0530

    helloworld

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,22 +1,34 @@
 <?php
+
+
 namespace Jiva\HelloWorld\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-	protected $_pageFactory;
 
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory
-		)
-	{
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+    protected $resultPageFactory;
 
-	public function execute()
-	{
-		die("Module Installed successfully");
-		return $this->_pageFactory->create();
-	}
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
 }
